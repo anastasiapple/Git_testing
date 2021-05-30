969bd9e0349b14f1861ce0071a6dc7b388b4eb8b
Red-Caesar <suslovavd2002@mail.ru>
2021-05-31 00:55:38 +0300

    Add files

--- main.c
+++ main.c
@@ -2,8 +2,15 @@
 #include "fun.h"
 
 int main(){
+    FILE *in, *out;
+    in = fopen("input.txt", "r");
+    out = fopen("output.txt", "w");
+
     printf("Hello world!\n");
     printf("Number: %d/n", sum(5,6));
     printf("Number: %d", mult(5,6));
+
+    fclose(in);
+    fclose(out);
     return 0;
 }
