9b6dbb55e052153d8c4555a9f8e00348cb13f35d
Red-Caesar <suslovavd2002@mail.ru>
2021-05-30 23:11:47 +0300

    Start of work x2

---
+++ fun.h
@@ -0,0 +1,8 @@
+
+#ifndef GIT_TESTING_FUN_H
+#define GIT_TESTING_FUN_H
+
+int sum(int a, int b);
+
+
+#endif //GIT_TESTING_FUN_H
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "fun.h"
 
 int main(){
     printf("Hello world!\n");
+    printf("Number: %d", sum(5,6));
     return 0;
 }
